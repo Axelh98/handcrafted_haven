0b1f42b2196218d0686e43d51f2edaf8472b6288
Axel Herrera <73140275+Axelh98@users.noreply.github.com>
2025-04-09 17:46:53 -0300

    Update useClientReviews.ts

--- src/app/hooks/useClientReviews.ts
+++ src/app/hooks/useClientReviews.ts
@@ -2,45 +2,52 @@
 
 import { useEffect, useState } from 'react';
 
-export function useClientReviews(id: string) {
-	const [reviews, setReviews] = useState([]);
-	const [iteration, setIteration] = useState(1);
-
-	const [totalPages, setTotalPages] = useState(1);
-
-	useEffect(() => {
-		async function getTotalPages() {
-			const res = await fetch(`/api/products/${id}/reviews/pages`, {
-				headers: { 'Content-Type': 'application/json' }
-			});
-			const data = await res.json();
-
-			setTotalPages(data);
-		}
-
-		getTotalPages();
-	}, []);
-
-	useEffect(() => {
-		async function getCategories() {
-			if (iteration <= totalPages) {
-				const res = await fetch(`/api/products/${id}/reviews/${iteration}`, {
-					headers: { 'Content-Type': 'application/json' }
-				});
-				const data = await res.json();
-
-				setReviews((prevState) => {
-					return [...prevState, ...data];
-				});
-			}
-		}
-
-		getCategories();
-	}, [iteration]);
-
-	const reqMoreReviews = () => {
-		setIteration((prevState) => prevState + 1);
-	};
-
-	return { reviews, reqMoreReviews, isLast: iteration === totalPages };
+interface Review {
+  // Define the structure of a review object
+  id: string;
+  content: string;
+  // Add other relevant fields
 }
+
+export function useClientReviews(id: string) {
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [iteration, setIteration] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    async function getTotalPages() {
+      const res = await fetch(`/api/products/${id}/reviews/pages`, {
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const data = await res.json();
+
+      setTotalPages(data);
+    }
+
+    getTotalPages();
+  }, [id]);
+
+  useEffect(() => {
+    async function getCategories() {
+      if (iteration <= totalPages) {
+        const res = await fetch(`/api/products/${id}/reviews/${iteration}`, {
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await res.json();
+
+        setReviews((prevState) => {
+          return [...prevState, ...data];
+        });
+      }
+    }
+
+    getCategories();
+  }, [iteration, totalPages, id]);
+
+  const reqMoreReviews = () => {
+    setIteration((prevState) => prevState + 1);
+  };
+
+  return { reviews, reqMoreReviews, isLast: iteration === totalPages };
+}
+
