f4fab5dbb9af83e7b78f7ccaf724e23341b9d0ba
Vicente <vicenteferrer_01@hotmail.com>
2025-04-09 22:03:50 -0400

    Carousel Responsiveness

--- src/app/ui/Carousel.tsx
+++ src/app/ui/Carousel.tsx
@@ -18,7 +18,16 @@ export default function Carousel({ params }: Props) {
 		speed: 500,
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		autoplay: true
+		autoplay: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
 	};
 
 	return (
