9dd553868bb80284d06a86242b50dc92fe5d8bfe
Axel Herrera <axelh260498@gmail.com>
2025-04-14 16:44:59 -0300

    merge conflicts solved

--- src/app/hooks/useClientGetProfile.ts
+++ src/app/hooks/useClientGetProfile.ts
@@ -8,26 +8,26 @@ export function useClientGetProfile(id: number) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-	useEffect(() => {
-		async function getProfile() {
-			try {
-				const res = await fetch(`/api/profiles?id=${id}`, {
-					headers: { 'Content-Type': 'application/json' }
-				});
-				const data = await res.json();
-				setProfile(data);
-			} catch (error) {
-				if (error instanceof Error) {
-					console.error('Error fetching profile:', error.message);
-					setError(error.message);
-				}
-			} finally {
-				setLoading(false);
-			}
-		}
+  useEffect(() => {
+    async function getProfile() {
+      try {
+        const res = await fetch(`/api/profiles?id=${id}`, {
+          headers: { 'Content-Type': 'application/json' }
+        });
+        const data = await res.json();
+        setProfile(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error('Error fetching profile:', error.message);
+          setError(error); // ✅ corregido: se pasa el objeto Error completo
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
 
     if (id) getProfile();
   }, [id]);
 
-	return { profile, loading, error };
+  return { profile, loading, error };
 }
