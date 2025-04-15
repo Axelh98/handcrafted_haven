c9505d378cc6e6b7d7141def104a96326c376222
Vicente <vicenteferrer_01@hotmail.com>
2025-04-14 22:06:11 -0400

    Seller Profile Styled

--- src/app/sellers/page.tsx
+++ src/app/sellers/page.tsx
@@ -1,11 +1,17 @@
+import { Metadata } from 'next';
+import VoidReturn from '../ui/VoidReturn';
 import Grid from '../ui/Grid';
 import { Search } from '../lib/definitions';
 import { fetchFilteredSellers } from '../lib/data';
-import gridStyle from '@/app/ui/sellers/grid.module.css';
+import custom from '@/app/ui/sellers/custom.module.css';
 import styles from '@/app/ui/sellers/sellers.module.css';
 
 type PageProps = {
 	searchParams?: Promise<Search>;
+};
+
+export const metadata: Metadata = {
+	title: 'Search Sellers'
 };
 
 export default async function Page(props: PageProps) {
@@ -13,9 +19,14 @@ export default async function Page(props: PageProps) {
 
 	const profiles = await fetchFilteredSellers(searchParams);
 
+	const isVoid = profiles.length === 0;
+
 	return (
 		<main className={styles.main}>
-			<Grid items={profiles} customStyle={gridStyle.customGrid} />
+			{isVoid && (
+				<VoidReturn message='No sellers found! Try again.' customStyle={custom.customError} />
+			)}
+			{!isVoid && <Grid items={profiles} customStyle={custom.customGrid} />}
 		</main>
 	);
 }
