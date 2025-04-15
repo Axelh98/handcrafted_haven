4a34742e22b73897d98b559ae0e2b8fe53b5ffd8
Axel Herrera <axelh260498@gmail.com>
2025-04-14 21:22:40 -0300

    animación al agregar un nuevo producto.

--- src/app/ui/cart/AddToCartButton.tsx
+++ src/app/ui/cart/AddToCartButton.tsx
@@ -1,24 +1,34 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useCart } from '@/app/context/CartContext';
+import "./Cart.css";
 
 type AddToCartButtonProps = {
   product: {
     id: string;
     name: string;
     price: number;
-    image_url: string; 
+    image_url: string;
   };
 };
 
 export default function AddToCartButton({ product }: AddToCartButtonProps) {
   const { addToCart } = useCart();
+  const [showMessage, setShowMessage] = useState(false);
 
   const handleClick = () => {
-    addToCart(product); 
-    alert(`${product.name} added to the cart!`);
+    addToCart(product);
+    setShowMessage(true);
+    setTimeout(() => setShowMessage(false), 1000);
   };
 
-  return <button onClick={handleClick}>Add to Cart</button>;
+  return (
+    <div className='wrapper'>
+      <button onClick={handleClick}>Add to Cart</button>
+      {showMessage && (
+        <span className='message'>✔ Added to cart!</span>
+      )}
+    </div>
+  );
 }
